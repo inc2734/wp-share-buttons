ae7308ab174b4c2b3abe7e4959dbcf8a04f0a30e
Takashi Kitajima <inc@2inc.org>
2018-01-26 05:28:01 +0900

    Fix official tweet button bug

--- src/view/twitter/official.php
+++ src/view/twitter/official.php
@@ -5,5 +5,5 @@
  * @license GPL-2.0+
  */
 ?>
-<a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php echo esc_attr( urlencode( get_permalink( $post_id ) ) ); ?>" data-text="<?php echo esc_attr( urlencode( $title ) ); ?>"><?php esc_html_e( 'Tweet', 'inc2734-wp-share-buttons' ); ?></a>
+<a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php echo esc_attr( get_permalink( $post_id ) ); ?>" data-text="<?php echo esc_attr( $title ); ?>"><?php esc_html_e( 'Tweet', 'inc2734-wp-share-buttons' ); ?></a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
