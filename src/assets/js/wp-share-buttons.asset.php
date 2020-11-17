673d7e175c519c638ec7679f41f9d4e599030b56
inc2734 <inc@2inc.org>
2020-11-17 10:24:37 +0900

    https://github.com/inc2734/snow-monkey/issues/222

--- src/assets/js/wp-share-buttons.asset.php
+++ src/assets/js/wp-share-buttons.asset.php
@@ -1,4 +1,1 @@
-<?php return array(
-	'dependencies' => array( 'wp-polyfill' ),
-	'version'      => 'dacc12c5179de7f5289612c4494ae2f5',
-);
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'dacc12c5179de7f5289612c4494ae2f5');
