f917f8c90949da99473a4cf29e9af3cfbc68f906
inc2734 <inc@2inc.org>
2023-11-27 15:19:12 +0900

    Fix #4

--- src/view/pinterest/official.php
+++ src/view/pinterest/official.php
@@ -1,2 +1,9 @@
+<?php
+/**
+ * @package inc2734/wp-share-buttons
+ * @author inc2734
+ * @license GPL-2.0+
+ */
+?>
 <a href="https://www.pinterest.com/pin/create/button/" data-pin-do="buttonBookmark"></a>
 <script>!function(d,i){if(!d.getElementById(i)){var j=d.createElement("script");j.id=i;j.src="https://assets.pinterest.com/js/pinit_main.js";var w=d.getElementById(i);d.body.appendChild(j);}}(document,"pinterest-btn-js");</script>
